c5df08f83172a45312ae7c23e8fa3a930dfa99b1
Antoine Phan <antoine.phan@mail.mcgill.ca>
2025-01-28 10:52:33 -0500

    Splititng E1 into tasks

---
+++ Lab/Lab1/E1_T1/E1_T1.ino
@@ -0,0 +1,36 @@
+// Setting the location of the digital pins
+#define R_PIN 3
+#define G_PIN 5
+#define B_PIN 6
+
+/**
+ * Write color in RGB value
+ * @param r (0,1)
+ */
+void setColor(int r, int g, int b)
+{
+    digitalWrite(R_PIN, r);
+    digitalWrite(G_PIN, g);
+    digitalWrite(B_PIN, b);
+}
+
+void task1()
+{
+    setColor(1, 1, 1);
+    delay(500);
+    setColor(0, 0, 0);
+    delay(500);
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(R_PIN, OUTPUT);
+    pinMode(G_PIN, OUTPUT);
+    pinMode(B_PIN, OUTPUT);
+}
+
+void loop()
+{
+    // task1();
+}
